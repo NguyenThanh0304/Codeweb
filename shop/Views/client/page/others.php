a20690d91abccbf8d5f7cc5a54e767b57340a7bc
NGUYEN CHI THANH <32605338+NguyenThanh0304@users.noreply.github.com>
2018-01-14 20:39:29 +0700

    update others,php
    chinh sua code phần client pages

--- shop/Views/client/page/others.php
+++ shop/Views/client/page/others.php
@@ -10,13 +10,13 @@
 
     <title>Other</title>
 <?php include_once 'header.php'; ?>
-        <div class="row" style="padding: 5px;background-color: black">
-          <h4 style="color: white;padding-left: 20px">Phụ kiện
-            <a class="" style="padding-left: 750px;font-size: 15px"> Chuột</a>
-            <a class="" style="padding-left: 20px;font-size: 15px"> Loa</a>
-            <a class=""  style="padding-left: 20px;font-size: 15px"> Bàn phím</a>
-            <a class="" style="padding-left: 20px;font-size: 15px"> Tai nghe</a>
-             <a class="" style="padding-left: 20px;font-size: 15px"> Usb</a>
+        <div class="row" style="padding: 5px;background-color: #215f8a;">
+          <h4 style="color: white;padding-left: 20px">PHỤ KIỆN
+            <a class="" style="padding-left: 700px;font-size: 15px"> CHUỘT</a>
+            <a class="" style="padding-left: 20px;font-size: 15px"> LOA</a>
+            <a class=""  style="padding-left: 20px;font-size: 15px"> BÀN PHÍM</a>
+            <a class="" style="padding-left: 20px;font-size: 15px"> TAI NGHE</a>
+             <a class="" style="padding-left: 20px;font-size: 15px"> USB</a>
           </h4>
         </div>
         <?php 
@@ -34,7 +34,7 @@
                 <p style="text-decoration: line-through;"><i>Giá cũ: <?php echo number_format($listothers['price']) ?></i></p>
                 <p style="color: red">Giá mới: <?php echo number_format($listothers['price'] * 0.9); ?></p>
                 <p><b> <?php echo $listothers['name']; ?>  </b></p> 
-                <a href="insertcart.php?id=<?php echo $listothers['id'] ?>&&idproducts=<?php echo $listothers['idproducts'] ?>">Thêm vào giỏ hàng</a>  
+                <a href="insertcart.php?id=<?php echo $listothers['id'] ?>&&idproducts=<?php echo $listothers['idproducts'] ?>"><img src="../../../public/images/cart1.jpg" alt="" width="36%" style="margin-left: 30px"></a>  
               </div>
               <div class="card-footer">
                 <small class="text-muted"><a style="color: yellow;font-size: 20px">&#9733; &#9733; &#9733; &#9733; &#9734;</a>(10 đánh giá)</small>
@@ -44,4 +44,4 @@
         <?php endforeach; ?>
       </div>
 
-<?php include_once(dirname(__FILE__).'/footer.php'); ?>sss
+<?php include_once(dirname(__FILE__).'/footer1.php'); ?>
